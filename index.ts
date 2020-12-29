5b4b61181ff86d991e0578dbd37e9ecf3ea439d6
analopesdev <analopesdemorais@outlook.com>
2020-12-29 13:56:01 -0300

    first commit

---
+++ index.ts
@@ -0,0 +1,9 @@
+/* eslint-disable */
+//  Tipos básicos  (aqui ocorre inferência de tipos)
+let nome2: string = 'Ana'
+let idade: number = 12
+let adulto: boolean = true
+let simbolo: symbol = Symbol('qualquer symbol')
+// let big: bigint = 10n;
+
+
